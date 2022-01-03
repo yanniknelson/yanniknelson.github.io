c44f24dcaf4e9a5a93274310cb71ebb74ac6c917
yanniknelson <yannikdanielnelson@gmail.com>
2022-01-03 04:46:47 +0000

    Implemented MinMax (no Pruning)
    Will implemented connect-4 to demonstrate pruning

--- Projects/TikTakToe/JavaScript/TikTakToe.js
+++ Projects/TikTakToe/JavaScript/TikTakToe.js
@@ -1,23 +1,159 @@
-player = 0
+currentPlayer = 0
+computerPlayer = 1
 board = []
+current_state = []
 
 function selectCellElement(cell) {
     if (cell.childElementCount > 0){
         return;
     }
-    icon = document.createElement('div');
-    if (player == 0){
+    var x = parseInt(cell.id[0]);
+    var y = parseInt(cell.id[2]);
+    current_state[y*3 + x] = currentPlayer;
+    var icon = document.createElement('div');
+    if (currentPlayer == 0){
         icon.classList.add("nought");
-        player = 1
+        currentPlayer = 1
     } else {
         icon.classList.add("cross");
-        player = 0
+        currentPlayer = 0
     }
     cell.appendChild(icon)
+    if (currentPlayer == computerPlayer && !isTerminal(current_state)) {
+        var move = minmax(current_state);
+        selectCell(move % 3, Math.floor(move/3));
+    }
 }
 
 function selectCell(x,y) {
     selectCellElement(document.getElementById(`${x},${y}`));
+}
+
+function isTerminal(state) {
+    //check diagonals
+    if (state[0] == state[4] && state[4] == state[8] && state[4] != -1) {
+        return true;
+    }
+    if (state[6] == state[4] && state[4] == state[2] && state[4] != -1) {
+        return true;
+    }
+    var sawempty = false
+    for (var i = 0; i < 3; i++) {
+        if (state[i*3] == -1 || state[i*3 + 1] == -1 || state[i*3 + 2] == -1) {
+            sawempty = true
+        }
+        //check rows
+        if (state[i*3] == state[i*3 + 1] && state[i*3 + 1] == state[i*3 + 2] && state[i*3 + 1] != -1) {
+            return true;
+        }
+        //check columns
+        if (state[i] == state[3 + i] && state[3 + i] == state[6 + i] && state[3 + i] != -1) {
+            return true;
+        }
+    }
+    //if we saw an empty space, the game isn't over, otherwise the game is over (and it was a draw)
+    return !sawempty;
+}
+
+function eval(state) {
+    if (state[0] == state[4] && state[4] == state[8]) {
+        if (state[4] == computerPlayer) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    if (state[6] == state[4] && state[4] == state[2]) {
+        if (state[4] == computerPlayer) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    for (var i = 0; i < 3; i++) {
+        //check rows
+        if (state[i*3] == state[i*3 + 1] && state[i*3 + 1] == state[i*3 + 2]) {
+            if (state[i*3 + 1] == computerPlayer) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+        //check columns
+        if (state[i] == state[3 + i] && state[3 + i] == state[6 + i]) {
+            if (state[3 + i] == computerPlayer) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+    //must be a draw
+    return 0;
+}
+
+function result(state, action, player) {
+    //return board state after action (x,y coord for given player)
+    var res = [...state];
+    res[action] = player;
+    return res;
+}
+
+function getActions(state) {
+    //return list of possible next moves (in x,y coordinates)
+    var actions = []
+    for (var i = 0; i < 9; i++) {
+        if (state[i] == -1) {
+            actions.push(i);
+        }
+    }
+    return actions;
+}
+
+function minmax(state) {
+    var actions = getActions(state);
+    var costs = []
+    for (var i = 0; i < actions.length; i++) {
+        costs.push(min(result(state, actions[i], computerPlayer)));
+    }
+    var index = 0;
+    for (var i = 0; i < costs.length; i++) {
+        if (costs[i] > costs[index]) {
+            index = i;
+        }
+    }
+    return actions[index];
+}
+
+function min(state) {
+    if (isTerminal(state)) {
+        return eval(state);
+    }
+    var v = Infinity;
+    var possibleActions = getActions(state);
+    for (var i = 0; i < possibleActions.length; i++) {
+        v = Math.min(v, max(result(state, possibleActions[i], (computerPlayer + 1) % 2)));
+    }
+    return v;
+}
+
+function max(state) {
+    if (isTerminal(state)) {
+        return eval(state);
+    }
+    var v = -Infinity;
+    var possibleActions = getActions(state);
+    for (var i = 0; i < possibleActions.length; i++) {
+        v = Math.max(v, min(result(state, possibleActions[i], computerPlayer)));
+    }
+    return v;
+}
+
+function resetGame() {
+    current_state = []
+    for (var i = 0; i < 9; i++) {
+            current_state.push(-1);
+    }
 }
 
 window.onload = function () {
@@ -35,5 +171,6 @@ window.onload = function () {
         }
         board.push(row);
     }
+    resetGame();
 }
 
